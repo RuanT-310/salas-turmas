a39b3884b1d4f2d6aa70108e080ef5c8ecd15eb7
Samillymoraes <samilly.moraes@unifesspa.edu.br>
2025-10-15 11:15:40 -0300

    tela home

--- app-comprar/src/app/Home/styles.ts
+++ app-comprar/src/app/Home/styles.ts
@@ -1,61 +1,30 @@
-import { StyleSheet } from "react-native"
+import { StyleSheet } from "react-native";
+import { THEME } from "@/theme";
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    backgroundColor: '#d0d2d8',
-    paddingTop: 62,
+    backgroundColor: THEME.COLORS.GRAY_600,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingHorizontal: 24,
   },
-  logo: {
-    height: 34,
-    width: 134,
+  icon: {
+    width: 56,
+    height: 56,
+    marginBottom: 32,
   },
-  form: {
-    width: '100%',
-    paddingHorizontal: 16,
-    gap: 7,
-    marginTop: 42,
+  title: {
+    fontSize: THEME.FONT_SIZE.XL,
+    fontFamily: THEME.FONT_FAMILY.BOLD,
+    color: THEME.COLORS.WHITE,
+    textAlign: "center",
   },
-  content: {
-    flex: 1,
-    width: '100%',
-    backgroundColor: '#FFFFFF',
-    borderTopLeftRadius: 24,
-    borderTopRightRadius: 24,
-    padding: 24,
-    paddingTop: 32,
-    marginTop: 24,
-  },
-  header: {
-    width: '100%',
-    flexDirection: 'row',
-    gap: 12,
-    borderBottomWidth: 1,
-    borderBottomColor: '#E4E6EC',
-    paddingBottom: 12,
-  },
-  clearButton: {
-    marginLeft: 'auto',
-  },
-  clearText: {
-    fontSize: 12,
-    color: '#828282',
-    fontWeight: 600,
-  },
-  separator: {
-    width: "100%",
-    height: 1,
-    backgroundColor: "#EEF0F5",
-    marginVertical: 16,
-  },
-  listContent: {
-    paddingTop: 24,
-    paddingBottom: 62,
-  },
-  empty: {
-    fontSize: 14,
-    color: "#808080",
-    textAlign: 'center'
+  subtitle: {
+    color: THEME.COLORS.GRAY_300,
+    fontSize: THEME.FONT_SIZE.SM,
+    fontFamily: THEME.FONT_FAMILY.REGULAR,
+    textAlign: "center",
+    marginBottom: 32,
   },
 });
